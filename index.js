2462c2824c550ff538af2d9e30b8d2316e0d693c
Ronny A. Nilsen <rn@ronnynilsen.com>
2019-03-13 22:39:23 +0100

    Index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const requestHandler = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World!');
+}
+const server = http.createServer(requestHandler);
+server.listen(8080);
