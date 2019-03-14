146743e36d5cd014a770bb022227e257c9bbb81e
Ronny A. Nilsen <rn@ronnynilsen.com>
2019-03-14 22:57:02 +0100

    Minnor.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ const requestHandler = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, World!');
 }
+
 const server = http.createServer(requestHandler);
 server.listen(8080);
