8b64b188b055a5254d79fe702da73f4d0925bdd6
Ronny A. Nilsen <rn@ronnynilsen.com>
2019-03-14 21:38:15 +0100

    Added build settings etc.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const http = require('http');
+import "@babel/polyfill";
+import http from 'http';
+
 const requestHandler = function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, World!');
